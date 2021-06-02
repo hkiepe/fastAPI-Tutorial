8160b46fd9315633841a7ba6b6a85ef07fec05fe
Henrik Kiepe <hkiepe@inkontor.com>
2021-06-02 21:28:57 +0200

    created the database connections

--- blog/database.py
+++ blog/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
