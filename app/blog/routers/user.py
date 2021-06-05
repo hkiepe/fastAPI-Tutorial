b6ac3a1328e38b316c535bf177c24c19588c1f1d
Henrik Kiepe <hkiepe@inkontor.com>
2021-06-05 21:29:31 +0200

    deployed app

---
+++ app/blog/routers/user.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from blog import schemas, database
+from typing import List
+from sqlalchemy.orm import Session
+from blog.repository import user
+
+
+router = APIRouter(
+    prefix="/user",
+    tags = ['Users']
+)
+
+get_db = database.get_db
+
+
+@router.post('/', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create(request, db)
+
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    return user.show(id, db)
